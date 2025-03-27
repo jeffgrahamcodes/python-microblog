81f2d527d3300ea2c23a3f9dc121d5e470b8884a
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-27 16:48:56 -0400

    store entries in MongoDB

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, request
 import datetime
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from urllib.parse import quote_plus
 
 app = Flask(__name__)
 
+load_dotenv()
+
+username = quote_plus(os.getenv("MONGO_USERNAME"))
+password = quote_plus(os.getenv("MONGO_PASSWORD"))
+host = os.getenv("MONGO_HOST")
+dbname = os.getenv("MONGO_DBNAME")
+
+mongo_uri = f"mongodb+srv://{username}:{password}@python-microblog.1kbff8y.mongodb.net/"
+
+client = MongoClient(mongo_uri)
+app.db = client.microblog
 entries = []
 
 
@@ -12,4 +27,14 @@ def home():
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.insert(0, (entry_content, formatted_date))
-    return render_template("home.html", entries=entries)
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"),
+        )
+        for entry in entries
+    ]
+    return render_template("home.html", entries=entries_with_date)
